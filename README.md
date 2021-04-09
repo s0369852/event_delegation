819463441afd33884326c3629c4980335990235f
s0369852 <s0369852@gmail.com>
2021-04-09 14:14:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-esnqss
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-esnqss)
---
+++ event_delegation.js
@@ -0,0 +1,8 @@
+var inputs = document.querySelectorAll("input");
+inputs.forEach(function(input) {
+  input.addEventListener("click", function(event) {
+    alert("checked");
+  });
+});
+
+// 새 리스트 아이템을 추가하는 코드
